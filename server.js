9424268fa7e442b64a0590064ee23f7311d51332
seatpi13600-dev <seatpi13600@gmail.com>
2026-07-11 12:00:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static(__dirname));
 
